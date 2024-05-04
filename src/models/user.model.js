4f1b9d6535c9f425f63fb493c9943d63075c0c09
Nicolas <niko_hervith@hotmail.com>
2024-05-04 15:25:28 -0300

    rol added to user schema

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -16,6 +16,11 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    role: {
+      type: String,
+      enum: ["user", "admin", "creator"], // Define los roles permitidos
+      default: "user", // Valor por defecto
+    },
   },
   {
     timestamps: true,
--- src/schemas/auth.schema.js
+++ src/schemas/auth.schema.js
@@ -18,6 +18,7 @@ export const registerSchema = z.object({
     .min(6, {
       message: "Password must be at least 6 characters",
     }),
+  role: z.enum(["user", "admin", "creator"]).optional(),
 });
 
 export const loginSchema = z.object({
