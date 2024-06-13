837e6fc342e8d546159828f621942f85e140daf5
Nicolas <niko_hervith@hotmail.com>
2024-06-12 22:06:06 -0300

    multer config

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from "express";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import videoRoutes from "./routes/videoRoutes.js";
 
 import authRoutes from "./routes/auth.routes.js";
 
@@ -18,5 +19,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+// Rutas de video
+app.use("/api/video", videoRoutes);
 
 export default app;
---
+++ src/config/multer-config.js
@@ -0,0 +1,39 @@
+import multer from "multer";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    if (file.mimetype.startsWith("video/")) {
+      cb(null, "uploads/videos"); // Asegúrate de que esta carpeta exista
+    } else if (file.mimetype.startsWith("image/")) {
+      cb(null, "uploads/images"); // Asegúrate de que esta carpeta exista
+    } else {
+      cb(new Error("Solo se permiten archivos de video o imagen"), false);
+    }
+  },
+  filename: (req, file, cb) => {
+    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+    cb(
+      null,
+      file.fieldname + "-" + uniqueSuffix + path.extname(file.originalname)
+    );
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype.startsWith("video/") ||
+    file.mimetype.startsWith("image/")
+  ) {
+    cb(null, true);
+  } else {
+    cb(new Error("Solo se permiten archivos de video o imagen"), false);
+  }
+};
+
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+});
+
+export default upload;
