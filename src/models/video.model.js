3c3fad1fb4688b6d8b278f86087404c832f3835a
Nicolas <niko_hervith@hotmail.com>
2024-06-12 22:02:32 -0300

    video schema

---
+++ src/models/video.model.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const videoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tags: {
+    type: [String],
+    default: [],
+  },
+  videoUrl: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+export default mongoose.model("Video", videoSchema);
